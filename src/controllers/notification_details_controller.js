4e6ca1385fa547d44d4db76245ab49eb3734ecdb
nghimartin <soobinnghi@gmail.com>
2024-11-02 12:19:24 +0700

    update mark-read  notification

--- src/controllers/notification_details_controller.js
+++ src/controllers/notification_details_controller.js
@@ -59,7 +59,59 @@ console.log({userId: userId, notificationDetailsId: notificationDetailsId});
   });
 });
 
+const getUnreadNotificationCount = asyncHandler(async (req, res) => {
+  const { userId } = req.query;
+
+  // Tìm thông báo dựa trên userId và kiểm tra xem có thông báo nào chưa đọc không
+  const notification = await Notification.findOne({ userId }).populate({
+    path: 'notificationDetails',
+    match: { notiStatus: 'unread' }  // Chỉ lấy các thông báo có status là "unread"
+  });
+
+  if (!notification) {
+    return res.status(404).json({
+      status: 404,
+      message: 'Không tìm thấy thông báo cho userId này',
+    });
+  }
+
+  // Đếm số lượng thông báo chưa đọc
+  const unreadCount = notification.notificationDetails.length;
+
+  res.json({
+    status: 200,
+    message: 'Số lượng thông báo chưa đọc',
+    data: unreadCount,
+  });
+});
+
+
+const markNotificationAsRead = asyncHandler(async (req, res) => {
+  const { userId, notificationDetailsId } = req.body;
+
+  // Tìm và cập nhật trạng thái thông báo thành "read" dựa trên userId và notificationDetailsId
+  const updatedNotification = await NotificationDetailsModel.findOneAndUpdate(
+    { _id: notificationDetailsId, userId: userId },
+    { notiStatus: 'read' },
+    { new: true } // Trả về document đã cập nhật
+  );
+
+  if (!updatedNotification) {
+    return res.status(404).json({
+      status: 404,
+      message: 'Không tìm thấy thông báo để cập nhật cho userId này',
+    });
+  }
+
+  res.json({
+    status: 200,
+    message: 'Cập nhật trạng thái thông báo thành "read" thành công',
+    data: updatedNotification,
+  });
+});
 module.exports = {
   deleteNotificationDetailById,
   getNotificationDetailsByUserId,
+  getUnreadNotificationCount,
+  markNotificationAsRead
 };
--- src/routers/notification_router.js
+++ src/routers/notification_router.js
@@ -1,9 +1,11 @@
 const Router = require('express');
 
-const { getNotificationDetailsByUserId, deleteNotificationDetailById } = require('../controllers/notification_details_controller');
+const { getNotificationDetailsByUserId, deleteNotificationDetailById, getUnreadNotificationCount, markNotificationAsRead } = require('../controllers/notification_details_controller');
 var NotificationRouter = Router();
 
 NotificationRouter.get('/get-alls', getNotificationDetailsByUserId);
+NotificationRouter.get('/get-count-unread', getUnreadNotificationCount);
 NotificationRouter.delete('/delete', deleteNotificationDetailById);
+NotificationRouter.patch('/update-mark-read', markNotificationAsRead);
 
 module.exports = NotificationRouter;
