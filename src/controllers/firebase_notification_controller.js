457d78883521397e8c70eb42e1c1172530e07411
nghimartin <soobinnghi@gmail.com>
2024-12-02 17:49:53 +0700

    remove comment

--- src/controllers/firebase_notification_controller.js
+++ src/controllers/firebase_notification_controller.js
@@ -5,97 +5,6 @@ const asyncHandler = require("express-async-handler");
 const { default: mongoose } = require('mongoose');
 const PaymentModel = require("../models/payment_model");
 
-// const sendFirebaseNotification = asyncHandler(async (req, res) => {
-//   const { userId, sender, title, body, shortDescription, notification_type, object_type_id } = req.body;
-
-//   let recipientUserId = null;
-//   let recipientUser = null;
-//   let senderId = null;
-//   try {
-//     if (notification_type === 'order_update' && !userId) {
-    
-//       const payment = await PaymentModel.findById(object_type_id).populate({
-//         path: 'id_cart',
-//         populate: { path: 'id_product', select: 'id_user' },
-//       });
-
-//       if (!payment) {
-//         return res.status(404).json({ message: 'Payment not found.', success: false });
-//       }
-
-//       const cart = payment?.id_cart[0];
-//       const product = cart?.id_product;
-
-//       if (!product?.id_user) {
-//         return res.status(404).json({ message: 'Product or shop not found.', success: false });
-//       }
-      
-//       recipientUserId = product?.id_user;
-//     } else if(notification_type === 'order_update' && userId) {
-//       const payment = await PaymentModel.findById(object_type_id).populate({
-//         path: 'id_cart',
-//         populate: { path: 'id_product', select: 'id_user' },
-//       });
-//       if (!payment) {
-//         return res.status(404).json({ message: 'Payment not found.', success: false });
-//       }
-//       const cart = payment?.id_cart[0];
-//       const product = cart?.id_product;
-//       if (!product?.id_user) {
-//         return res.status(404).json({ message: 'Product or shop not found.', success: false });
-//       }
-//       senderId = product?.id_user;
-//       recipientUserId = userId;
-//     } else {
-//       recipientUserId = userId;
-//     }
-
-  
-//     recipientUser = await User.findById(recipientUserId);
-
-//     if (!recipientUser) {
-//       return res.status(404).json({ message: 'Recipient user not found.', success: false });
-//     }
-
-//     // Tạo thông báo trong DB
-//     const newNotification = new Notification({
-//       recipient: recipientUserId,
-//       sender : senderId ?  senderId : sender,
-//       notification_type,
-//       title,
-//       body,
-//       object_type_id,
-//       shortDescription,
-//       status: "unread",
-//     });
-
-//     await newNotification.save();
-
-//     // Gửi thông báo qua Firebase nếu có `device_token`
-//     if (recipientUser.device_token) {
-//       const result = await NotificationService.sendNotification(
-//         recipientUser.device_token,
-//         title,
-//         body
-//       );
-
-//       return res.status(200).json({
-//         message: "Notification sent successfully via Firebase and saved in DB",
-//         success: true,
-//       });
-//     }
-
-//     console.warn(`User ${recipientUserId} does not have an fcmToken, skipping Firebase notification.`);
-//     return res.status(200).json({
-//       message: "Notification saved in DB, but Firebase notification was not sent (no fcmToken).",
-//       success: true,
-//     });
-//   } catch (error) {
-//     console.error("Error sending notification:", error);
-    
-//   }
-// });
-
 const sendFirebaseNotification = asyncHandler(async (req, res) => {
   const { userId, sender, listShopId, title, body, shortDescription, notification_type, object_type_id } = req.body;
 
@@ -103,9 +12,7 @@ const sendFirebaseNotification = asyncHandler(async (req, res) => {
     let recipientIds = [];
     let senderId = sender;
     console.log("listShopId Line 105 : ", listShopId);
-    // Xử lý nếu có `listShopId`
     if (Array.isArray(listShopId) && listShopId.length > 0) {
-      // Lấy danh sách userId (chủ shop) dựa trên listShopId
       const shopOwners = await User.find({
         _id: { $in: listShopId },
       }).select("_id device_token");
@@ -120,7 +27,6 @@ const sendFirebaseNotification = asyncHandler(async (req, res) => {
         deviceToken: shop.device_token,
       }));
     } else if (userId) {
-      // Nếu không có `listShopId`, sử dụng `userId`
       const user = await User.findById(userId).select("_id device_token");
 
       if (!user) {
@@ -135,12 +41,10 @@ const sendFirebaseNotification = asyncHandler(async (req, res) => {
       return res.status(400).json({ message: "Either listShopId or userId must be provided.", success: false });
     }
 
-    // Tạo thông báo và gửi Firebase notification cho từng recipient
     const notifications = [];
     for (const recipient of recipientIds) {
       const { id, deviceToken } = recipient;
 
-      // Tạo bản ghi notification trong database
       const newNotification = new Notification({
         recipient: id,
         sender: senderId,
@@ -155,7 +59,6 @@ const sendFirebaseNotification = asyncHandler(async (req, res) => {
       await newNotification.save();
       notifications.push(newNotification);
 
-      // Gửi notification qua Firebase
       if (deviceToken) {
         try {
           await NotificationService.sendNotification(deviceToken, title, body);
